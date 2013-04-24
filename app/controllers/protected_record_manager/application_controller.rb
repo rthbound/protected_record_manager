3c50b6fb1f983a68966b6e9288d8ee5f7447c0d8
Tad Hosford <tad.hosford@gmail.com>
2013-04-24 13:01:27 -0500

    Methods to disallow unauthorized users

--- app/controllers/protected_record_manager/application_controller.rb
+++ app/controllers/protected_record_manager/application_controller.rb
@@ -1,4 +1,11 @@
 module ProtectedRecordManager
   class ApplicationController < ActionController::Base
+    def ensure_record_manager
+      current_user.protected_record_manager || not_found
+    end
+
+    def not_found
+      render status: 403, text: 'Resource does not exist, or user lacks required permissions.' and return
+    end
   end
 end
