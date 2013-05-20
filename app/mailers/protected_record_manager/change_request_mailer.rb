364def5d7da58292f54077c57561742ee301a949
Tad Hosford <tad.hosford@gmail.com>
2013-05-20 14:59:19 -0500

    Send one email to all users (not indiv. email to each user)

--- app/mailers/protected_record_manager/change_request_mailer.rb
+++ app/mailers/protected_record_manager/change_request_mailer.rb
@@ -2,11 +2,10 @@ module ProtectedRecordManager
   class ChangeRequestMailer < ActionMailer::Base
     default from: ProtectedRecordManager::Configuration.default_from
 
-    def notify(user, change_request)
-      @user = user
+    def notify(users, change_request)
       @url  = change_request_url(change_request)
       mail({
-        to:      user.email,
+        to:      users.collect(&:email).join(","),
         subject: "New Change Request from #{change_request.user.email}"
       })
     end
